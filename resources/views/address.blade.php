40503a5345074bb87a25661815d0ce87d354feaf
liuweijie <liuweijie@example.com>
2026-09-15 21:57:03 +0900

    前端更改

--- resources/views/address.blade.php
+++ resources/views/address.blade.php
@@ -5,7 +5,7 @@
 <head>
 
 <meta charset="UTF-8">
-<link rel="icon" type="image/png" href="{{ asset('images/favicon.png') }}">
+<link rel="icon" href="{{ asset('images/favicon.png') }}" type="image/png">
 
 <meta
     name="viewport"
